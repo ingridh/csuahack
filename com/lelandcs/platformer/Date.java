9b5c3aab3c726a8b650d508f7796959a74a2d272
zhangaaron <aaronzcan@gmail.com>
2013-10-19 01:48:07 -0700

    Added fish and added Date

---
+++ com/lelandcs/platformer/Date.java
@@ -0,0 +1,42 @@
+package com.lelandcs.platformer;
+
+
+public class Date implements Comparable {
+	private String day; //1-31
+	private String hour;
+	private String month; // day = month * 24 * 31
+	public Date (String hour, String day, String month) {
+		this.day = day;
+		this.month = month;
+		this.hour = hour;
+	}
+	
+	public String getDay() {
+		return day;
+	}
+	public String getHour() {
+		return hour;
+	}
+	public String getMonth() {
+		return month;
+	}
+	
+	public int compareTo(Object object) {
+		Date date = (Date)object;
+		int hours1 = Integer.parseInt(hour) + 24 * Integer.parseInt(day) + 24 * 31 * Integer.parseInt(month); 
+		int hours2 = Integer.parseInt(date.hour) + 24 * Integer.parseInt(date.day) + 24 * 31 * Integer.parseInt(date.month); 
+		if (hours1 < hours2) {
+			return -1;
+		}
+		if (hours1 > hours2) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	
+}
+	
+
